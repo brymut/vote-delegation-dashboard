d746671bd6677d0d228ae8bbf78e711ac5a0ad5d
Bryan Mutai <mutaiwork@gmail.com>
2022-05-17 15:03:33 +0300

    fix header

--- components/Navigation.js
+++ components/Navigation.js
@@ -16,10 +16,10 @@ function classNames(...classes) {
 
 export default function Navigation() {
     return (
-        <Disclosure as="nav" className="bg-white/50 mx-4 my-4 rounded-xl">
+        <Disclosure as="nav" className="bg-white/50 mx-20 my-8 rounded-xl">
             {({ open }) => (
                 <>
-                    <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                    <div className="mx-auto px-2 sm:px-6 lg:px-8">
                         <div className="relative flex items-center justify-between h-16">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
@@ -34,14 +34,14 @@ export default function Navigation() {
                             </div>
                             <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex-shrink-0 flex items-center">
-                                    <div className="block lg:hidden h-8 w-auto">
-                                        <Image src="/apw-text-transparent-color.png" alt="Vercel Logo" width={200} height={48.59} />
+                                    <div className="block lg:hidden pt-2  w-auto">
+                                        <Image src="/apw-logo-light-circle.png" alt="APWine Logo" width={50} height={50} />
                                     </div>
                                     <div className="hidden lg:block  w-auto">
-                                        <Image src="/apw-text-transparent-color.png" alt="Vercel Logo" width={175} height={42} />
+                                        <Image src="/apw-text-transparent-color.png" alt="APWine Logo" width={175} height={42} />
                                     </div>
                                 </div>
-                                <div className="hidden sm:block sm:ml-6 pt-2">
+                                <div className="hidden sm:block sm:ml-6 pt-5 lg:pt-3">
                                     <div className="flex space-x-4">
                                         {navigation.map((item) => (
                                             <a
@@ -57,13 +57,13 @@ export default function Navigation() {
                                 </div>
                             </div>
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                                <button
-                                    type="button"
-                                    className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                >
-                                    <LinkIcon className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
-                                    Connect Wallet
-                                </button>
+                                <div className="hidden sm:block" >
+                                    <button
+                                        type="button" className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" >
+                                        <LinkIcon className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
+                                        Connect Wallet
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -84,6 +84,12 @@ export default function Navigation() {
                                     {item.name}
                                 </Disclosure.Button>
                             ))}
+                            <div className="px-2">
+                                <button type="button" className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    <LinkIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+                                    Connect Wallet
+                                </button>
+                            </div>
                         </div>
                     </Disclosure.Panel>
                 </>
