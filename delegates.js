bebef64235d0288fb2ef528a772dc108deb3a55c
Bryan Mutai <mutaiwork@gmail.com>
2022-05-18 11:52:12 +0300

    update test twitter account

--- delegates.js
+++ delegates.js
@@ -4,7 +4,7 @@ export const delegates = [
     address: '0xE27F2E8321Fb4c32525a4ED86d2902dbA63491E4',
     bio: 'Went into crypto in 2016. Community builder @ early stage crypto startups. Building the Keep network and tBTC community. Early adopter of Paladin. Believe in what is best for Paladin in the long term and for contributors/community',
     discordId: 'BryanMutai#6546',
-    twitter: 'brymutai',
+    twitter: 'APWineFinance',
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
   },
@@ -13,7 +13,7 @@ export const delegates = [
     address: '0xE27F2E8321Fb4c32525a4ED86d2902dbA63491E4',
     bio: 'Went into crypto in 2016. Community builder @ early stage crypto startups. Building the Keep network and tBTC community. Early adopter of Paladin. Believe in what is best for Paladin in the long term and for contributors/community',
     discordId: 'BryanMutai#6546',
-    twitter: 'brymutai',
+    twitter: 'APWineFinance',
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
   },
@@ -22,7 +22,7 @@ export const delegates = [
     address: '0xE27F2E8321Fb4c32525a4ED86d2902dbA63491E4',
     bio: 'Went into crypto in 2016. Community builder @ early stage crypto startups. Building the Keep network and tBTC community. Early adopter of Paladin. Believe in what is best for Paladin in the long term and for contributors/community',
     discordId: 'BryanMutai#6546',
-    twitter: 'brymutai',
+    twitter: 'APWineFinance',
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
   },
@@ -31,7 +31,7 @@ export const delegates = [
     address: '0x86C62a0DD8690fa2Ba6e3E3D9A18D021dBd2565A',
     bio: 'Went into crypto in 2016. Community builder @ early stage crypto startups. Building the Keep network and tBTC community. Early adopter of Paladin. Believe in what is best for Paladin in the long term and for contributors/community',
     discordId: 'BryanMutai#6546',
-    twitter: 'brymutai',
+    twitter: 'APWineFinance',
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
   }
