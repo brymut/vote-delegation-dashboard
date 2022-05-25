84c90aa20155d0d9d558e6820295dc968a319f1b
dydymoon <69637819+dydymoon@users.noreply.github.com>
2022-05-25 13:08:26 +0200

    Update delegates.js

--- delegates.js
+++ delegates.js
@@ -11,11 +11,11 @@ export const delegates = [
   {
     name: 'Starny',
     address: '0x79603115Df2Ba00659ADC63192325CF104ca529C',
-    bio: 'Belgian lover of DeFi. My experience is just beginning as I recently had an opportunity open up to me as well as my involvement with Paladin which only makes me grow day by day. From my experience, from what I read in particular at Paladin, etc… I realize the importance of governance in DeFi, and I can also understand that some people do not have time to be fully interested in it as much as I or the other delegates, This is why I want to take good care of your voting power, because like you, I want APWine to be stronger and go further by taking the right decisions. I have nothing more to share with you to push you to delegate for me other than to trust my motivation and my involvement in DeFi and particularly projects like Paladin, Paraswap, APWine, Jarvis, etc… Delegation can be done on my ENS:Starny.eth',
+    bio: 'Belgian DeFi lover since 2020. I have a good experience in the governance from Paladin, Paraswap and more recently I am DeFi / Technical Writer at Mimo. From my experiencen I realized the importance of governance in DeFi, and I can also understand that some people do not have time to be fully interested in it as much as I or the other delegates, This is why I want to take care of your voting power, because like you, I want APWine to be stronger and go further by taking the right decisions. Delegation can be done on my ENS : Starny.eth',
     discordId: 'Starny#3390',
     twitter: 'StarnyDefi',
     imageUrl:
-      '',
+      'https://ipfs.io/ipfs/QmRk1MYVpBXVhAmzyoBonBWYvTyJoFwv9ZferdUQSRFvYM',
   },
   {
     name: 'Jane Cooper',
@@ -24,6 +24,6 @@ export const delegates = [
     discordId: 'BryanMutai#6546',
     twitter: 'APWineFinance',
     imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+      '',
   }
 ]
