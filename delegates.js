ed2f0366442ff80567a0b9f5af7ab27f2fd4b03e
dydymoon <69637819+dydymoon@users.noreply.github.com>
2022-05-25 12:22:10 +0200

    Update delegates.js

--- delegates.js
+++ delegates.js
@@ -1,5 +1,23 @@
 export const delegates = [
   {
+    name: 'CoolGuy',
+    address: '0xDcdB7cc659462b9602ee2F55333E6619C6EdF2Bb',
+    bio: 'I am in charge of DEFI when it comes to the Arizona State University(ASU) DAO which is associated with the Blockchain at ASU club. My ens address is coolguy.asudao.eth. I don’t have a specific voting ethos, but I am very open to discussion and others’ ideas. I have been a very active member of this discourse and the discord. My goal is to work with others to turn APW into the next great DEFI primitive like Curve, AAVE, MAKER, etc…',
+    discordId: 'Cool Guy #0311',
+    twitter: '?',
+    imageUrl:
+      '?',
+  },
+  {
+    name: 'Starny',
+    address: '0x79603115Df2Ba00659ADC63192325CF104ca529C',
+    bio: 'Belgian lover of DeFi. My experience is just beginning as I recently had an opportunity open up to me as well as my involvement with Paladin which only makes me grow day by day. From my experience, from what I read in particular at Paladin, etc… I realize the importance of governance in DeFi, and I can also understand that some people do not have time to be fully interested in it as much as I or the other delegates, This is why I want to take good care of your voting power, because like you, I want APWine to be stronger and go further by taking the right decisions. I have nothing more to share with you to push you to delegate for me other than to trust my motivation and my involvement in DeFi and particularly projects like Paladin, Paraswap, APWine, Jarvis, etc… Delegation can be done on my ENS:Starny.eth',
+    discordId: 'Starny#3390',
+    twitter: 'StarnyDefi',
+    imageUrl:
+      '?',
+  },
+  {
     name: 'Jane Cooper',
     address: '0xE27F2E8321Fb4c32525a4ED86d2902dbA63491E4',
     bio: 'Went into crypto in 2016. Community builder @ early stage crypto startups. Building the Keep network and tBTC community. Early adopter of Paladin. Believe in what is best for Paladin in the long term and for contributors/community',
@@ -8,31 +26,3 @@ export const delegates = [
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
   },
-  {
-    name: 'Jane Cooper',
-    address: '0xED26C400D2c4566b5Fe042BC9e807c297Ff3901C',
-    bio: 'Went into crypto in 2016. Community builder @ early stage crypto startups. Building the Keep network and tBTC community. Early adopter of Paladin. Believe in what is best for Paladin in the long term and for contributors/community',
-    discordId: 'BryanMutai#6546',
-    twitter: 'APWineFinance',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '0xE27F2E8321Fb4c32525a4ED86d2902dbA63491E4',
-    bio: 'Went into crypto in 2016. Community builder @ early stage crypto startups. Building the Keep network and tBTC community. Early adopter of Paladin. Believe in what is best for Paladin in the long term and for contributors/community',
-    discordId: 'BryanMutai#6546',
-    twitter: 'APWineFinance',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '0x86C62a0DD8690fa2Ba6e3E3D9A18D021dBd2565A',
-    bio: 'Went into crypto in 2016. Community builder @ early stage crypto startups. Building the Keep network and tBTC community. Early adopter of Paladin. Believe in what is best for Paladin in the long term and for contributors/community',
-    discordId: 'BryanMutai#6546',
-    twitter: 'APWineFinance',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  }
-]
